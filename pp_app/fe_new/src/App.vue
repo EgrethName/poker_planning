4bcd597456d1aec8499678d50dd24f8b40ea3ae0
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-09-02 15:10:37 +0300

    app: clean view

--- pp_app/fe_new/src/App.vue
+++ pp_app/fe_new/src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <router-view/>
-  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Footer from '@/components/Footer.vue';
 
 export default defineComponent({
-  components: {
-    Footer,
-  },
 });
 </script>
 
