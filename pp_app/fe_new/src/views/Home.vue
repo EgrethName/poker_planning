0a7d49822642917c367621b6e368bb547d0e68ac
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-09-02 15:10:58 +0300

    views: add footer in home & layout

--- pp_app/fe_new/src/views/Home.vue
+++ pp_app/fe_new/src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <Header />
-  </div>
+  <a-layout>
+    <a-layout-header>
+      <Header />
+    </a-layout-header>
+    <a-layout-footer>
+      <Footer />
+    </a-layout-footer>
+  </a-layout>
 </template>
 
 <script lang="ts">
@@ -10,12 +15,14 @@ import { defineComponent } from 'vue';
 import { mapState } from 'vuex';
 import { State } from '@/store';
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
 export default defineComponent({
   name: 'Home',
 
   components: {
     Header,
+    Footer,
   },
 
   computed: {
