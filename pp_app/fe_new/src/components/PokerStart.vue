2a8b9b8fb2f610aec5e91292433f9bdc444e3152
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-09-03 20:33:36 +0300

    finish start page

--- pp_app/fe_new/src/components/PokerStart.vue
+++ pp_app/fe_new/src/components/PokerStart.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="start-content">
-    Content
-    <create-game />
+    <div class="jumbotron">
+      <div class="jumbotron-header">
+        <h1>Poker Planning</h1>
+      </div>
+      <div class="jumbotron-body">
+        <h3>A simple app for agile poker planning</h3>
+      </div>
+      <div>
+        <h4>Create a game to start planning!</h4>
+      </div>
+      <create-game />
+    </div>
   </div>
 </template>
 
@@ -27,11 +37,48 @@ export default defineComponent({
 
 </script>
 
-<style scoped>
+<style
+  scoped
+  lang="scss"
+>
 
 .start-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   height: 100%;
   padding: 0 24px;
   background: #ffffff;
 }
+
+.jumbotron {
+  display: flex;
+  flex-direction: column;
+  //border: 1px dotted black;
+  margin: 20px;
+  background-color: #BDD0F7 !important;
+  border-radius: 10px;
+  padding: 4rem 2rem;
+
+  h1 {
+    font-weight: 500;
+    font-size: 4.5rem;
+  }
+
+  h3 {
+    font-weight: 400;
+    font-size: 1.5rem;
+  }
+
+  h4 {
+    font-weight: 300;
+    font-size: 1.25rem;
+  }
+
+  &-body {
+    margin-bottom: 0.2rem;
+  }
+
+}
+
 </style>
