10018d852b49c982c02478f40db2359c3336d814
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-09-06 01:14:27 +0300

    add input name modal - to refactor

---
+++ pp_app/fe_new/src/components/Game/InputNameModal.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="input-name">
+
+    <a-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Create a new game"
+      v-model:visible="visible"
+      @show="resetForm"
+      @hidden="resetForm"
+      @ok="onSubmit"
+      @cancel="resetForm"
+      ok-title="Create!"
+      cancel-variant="dark"
+      centered
+    >
+      <a-form
+        ref="formRef"
+        :model="formState"
+        :rules="rules"
+        @submit.stop.prevent="onSubmit"
+      >
+        <a-form-item
+          ref="name"
+          label="Your name"
+          name="name"
+        >
+          <a-input v-model:value="formState.name"/>
+        </a-form-item>
+      </a-form>
+    </a-modal>
+  </div>
+</template>
+
+<script lang="ts">
+// import { baseUrl, xhr } from '@/modules/xhr';
+import {
+  defineComponent,
+  reactive,
+  ref,
+  toRaw,
+  UnwrapRef,
+} from 'vue';
+import { notification } from 'ant-design-vue';
+import { ValidateErrorEntity } from 'ant-design-vue/es/form/interface';
+
+interface FormState {
+  name: string;
+}
+
+export default defineComponent({
+  name: 'CreateGame',
+  setup() {
+    const formRef = ref();
+    const visible = ref(false);
+    const formState: UnwrapRef<FormState> = reactive({
+      name: '',
+    });
+
+    const openNotification = () => {
+      notification.error({
+        message: 'Notification Title',
+        description:
+          'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        onClick: () => {
+          console.log('Notification Clicked!');
+        },
+      });
+    };
+
+    const rules = {
+      name: [
+        {
+          required: true, message: 'Please input Game name', trigger: 'blur',
+        },
+        {
+          min: 3, max: 20, message: 'Length should be 3 to 5', trigger: 'blur',
+        },
+      ],
+    };
+    const showModal = () => {
+      visible.value = true;
+    };
+    const resetForm = () => {
+      formRef.value.resetFields();
+    };
+    const onSubmit = (e: MouseEvent) => {
+      console.log(e);
+      formRef.value
+        .validate()
+        .then(() => {
+          console.log('values', formState, toRaw(formState));
+          visible.value = false;
+          resetForm();
+        })
+        .catch((error: ValidateErrorEntity<FormState>) => {
+          console.log('error', error);
+          openNotification();
+        });
+    };
+
+    return {
+      formRef,
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      other: '',
+      formState,
+      rules,
+      onSubmit,
+      resetForm,
+      visible,
+      showModal,
+      openNotification,
+    };
+  },
+});
+
+</script>
+
+<style
+  lang='scss'
+  scoped
+>
+.input-name {
+  top: 30%;
+  margin: auto;
+  padding: 2em;
+
+}
+</style>
