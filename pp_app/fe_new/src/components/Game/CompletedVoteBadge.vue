58d9d763ca4d0f97d7074eb9e7874eb1cc8bdc2c
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-09-06 01:07:39 +0300

    add completed vote badge

---
+++ pp_app/fe_new/src/components/Game/CompletedVoteBadge.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <button
+      class="completed-vote-badge"
+      @click="onClick"
+    >
+      {{ title }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CompletedVoteBadge',
+  props: {
+    stats: Object,
+  },
+  computed: {
+    title() {
+      return this.stats.title;
+    },
+    id() {
+      return this.stats.id;
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit('clicked', this.id);
+    },
+  },
+});
+</script>
+
+<style
+  scoped
+  lang="scss"
+>
+
+.completed-vote-badge {
+  -webkit-appearance: button;
+  background-color: #E8EEF9;
+  border: 2px solid #000;
+  border-radius: 5px;
+  box-sizing: border-box;
+  color: rgb(0, 0, 0);
+  cursor: pointer;
+  float: left;
+  font-weight: 700;
+  letter-spacing: .05em;
+  margin: 3px;
+  outline: none;
+  overflow: visible;
+  padding: 0.5em;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  text-transform: none;
+  transition: all .3s ease-in-out;
+  user-select: none;
+  width: 100%;
+
+  &:hover {
+    background-color: #BDD0F7;
+  }
+}
+
+</style>
